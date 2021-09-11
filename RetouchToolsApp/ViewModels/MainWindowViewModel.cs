555b58b5c6707b1a801c71155307f7b4fc92e92a
Anatoly Samokisha <anatoly@samokisha.com>
2021-09-11 23:23:57 +0300

    Init

---
+++ RetouchToolsApp/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RetouchToolsApp.ViewModels
+{
+    public class MainWindowViewModel : ViewModelBase
+    {
+        public string Greeting => "Welcome to Avalonia!";
+    }
+}
---
+++ RetouchToolsApp/ViewModels/ViewModelBase.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using ReactiveUI;
+
+namespace RetouchToolsApp.ViewModels
+{
+    public class ViewModelBase : ReactiveObject
+    {
+    }
+}
